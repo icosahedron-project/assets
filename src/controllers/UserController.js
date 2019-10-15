c9f591ee70b21c512c6e8561a280c382cc3d98c3
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2019-10-14 21:17:09 -0300

    Ready to hacktoberfest

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,46 +18,6 @@ function generateToken(params = {}) {
 
 // Exportando lógicas de negócio (requisições assíncronas) para routes.js
 module.exports = {
-
-    async info(req, res) {
-        const user = await User.findById(req.params.id);
-        return res.send({
-            name: user.name,
-            username: user.username
-        });
-    },
-
-    async login(req, res) {
-        const userRequest = {
-            username: req.params.username,
-            password: req.params.password
-        };
-        const user = await User.findOne(userRequest, (error, user) => {
-            return user;
-        });
-        return res.send({
-            user: user,
-            token: generateToken({ id: user.id })
-        });
-    },
-
-    // Gerando token ao registrar usuário
-    async register(req, res) {
-        const user = await User.create(req.body);
-        return res.send({
-            user: user,
-            token: generateToken({ id: user.id })
-        });
-    },
-
-    async update(req, res){
-        const user = await User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true});
-        return res.json(user);
-    },
-
-    async remove(req, res){
-        await User.findOneAndDelete({_id: req.params.id});
-        return res.send();    
-    }
+    // TO-DO business logic
 
 };
